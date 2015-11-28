26f9e6e6ca0cadf72e2d30957521ec4cb36adc5d
Kyle McGahee <kmcgahee@ksu.edu>
2015-11-28 10:54:09 -0600

    Set icon on window and task bar.

--- eeva_ui.py
+++ eeva_ui.py
@@ -1,4 +1,5 @@
 
+import os
 import sys
 from eeva_main_window import EevaMainWindow
 from eeva_controller import EevaController
@@ -10,6 +11,14 @@ if __name__ == '__main__':
 
     app = QtGui.QApplication(sys.argv)
     app.setStyle('plastique')
+    app.setWindowIcon(QtGui.QIcon('./resources/NERLogoTransparent.png'))
+    
+    if os.name == 'nt':
+        # If running on windows then need to unassociate process from python so that
+        # the OS will show the correct icon in the taskbar.
+        import ctypes
+        myappid = u'ner.eeva.ui.1' # arbitrary string
+        ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
     
     # Configure system
     link = GlobLink()
